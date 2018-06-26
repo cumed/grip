278cacabf66f9271e1ecdac41038e337ee45c800
cumed <cumedgithub@gmail.com>
2018-06-26 10:12:48 -0400

    Updated rotation threshold

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ directions = directions[::-1]
 servoDist_threshold       = 9.5                                                     # Max distance travelled by the back indexing servo(4.75*2)
 angle_threshold           = 0.01                                                          # Min angle required that the catheter needs to be bent by
 #neg_angle_threshold       = -1*angle_threshold
-rotationalAngle_threshold = 0                                                   # Min angle required that the catheter needs to be rotated by
+rotationalAngle_threshold = 2                                                   # Min angle required that the catheter needs to be rotated by
 incremental_distance      = 0                                                      # Keep track of distances until a bend is supposed to happen
 traversed_distance        = 0                                                        # Keep track of total distance
 
