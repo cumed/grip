bf9520795f9dac73880eaaf68be5d76d7044e8e0
cumed <cumedgithub@gmail.com>
2018-07-13 14:38:48 -0400

    a

--- factors.py
+++ factors.py
@@ -6,8 +6,8 @@ Created on Fri Jul 13 10:43:41 2018
 """
 
 bendPinsFactor  = 1.3
-angleRedFactor = input('Enter angle reduction factor') 
-distance_factor = input('Enter distance factor') 
+angleRedFactor = float(input('Enter angle reduction factor') )
+distance_factor = float(input('Enter distance factor'))
 y_i = 2.45
 d_pins = 5.25
 OD = 1.60
