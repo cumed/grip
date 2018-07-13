d668b553d90b064943d35930da2910e08bd05a96
cumed <cumedgithub@gmail.com>
2018-07-13 10:51:43 -0400

    a

--- skeleton_structure.py
+++ skeleton_structure.py
@@ -23,9 +23,10 @@ def remaining_distance(remDist,servoDist):
     return pulseDist
 
 # Pushing the catheter in front
+distance_factor=fact.distance_factor
 def push_catheter(servoDist_threshold, Dist, outer_diameter):
 #    if Dist > servoDist_threshold:                                           #Check if the pushing distance is more than the servo's threshold distance
-    distance_factor=fact.distance_factor
+    
     pulse_distance = remaining_distance(Dist,servoDist_threshold)        #Split it up into threshold distances if it is greater
     for rDistances in pulse_distance:
         print('****--Push catheter by '+str(rDistances*distance_factor) + 'mm from a total_distance of ' + str(Dist*distance_factor) + 'mm--****')
