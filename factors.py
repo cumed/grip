1b698c5725ff7ace67ce44a096eae3811b55af43
cumed <cumedgithub@gmail.com>
2018-07-19 09:55:02 -0400

    a

--- factors.py
+++ factors.py
@@ -4,13 +4,13 @@ Created on Fri Jul 13 10:43:41 2018
 
 @author: ATI-2 Pavan Gurudath
 """
-#bendPinsFactor = input('Enter bend pins factor?')
-bendPinsFactor = 1.4
+bendPinsFactor = input('Enter bend pins factor?')
+#bendPinsFactor = 1.4
 angleRedFactor = 1
 distanceFactor = 1
 y_i = 1.78
 d_pins = 5.25
-OD = 1.60
+OD = 1.62
 positiveAngleOffset = 0
 negativeAngleOffset = 0
 ODList = [OD,OD,OD,OD]
