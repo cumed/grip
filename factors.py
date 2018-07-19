0f0a8a4872744470a24fb674d42fd0910927fe99
cumed <cumedgithub@gmail.com>
2018-07-19 15:20:26 -0400

    Merged master clean

--- factors.py
+++ factors.py
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jul 13 10:43:41 2018
-
-@author: ATI-2 Pavan Gurudath
-"""
-#bendPinsFactor = input('Enter bend pins factor?')
-#bendPinsFactorPos = 1.6
-#bendPinsFactorNeg = 1.7
-bendPinsFactor = 1.65
-angleRedFactor = 1
-distanceFactor = 1
-y_i = 1.78
-d_pins = 5.25
-OD = 1.62
-positiveAngleOffset = 0
-negativeAngleOffset = 0
-ODList = [OD,OD,OD,OD]
-fudgepos = input('Enter fudge positive')
-fudgeneg = input('Enter fudge negative')
-=======
 # -*- coding: utf-8 -*-
 """
 Created on Fri Jul 15 10:43:41 2018
@@ -38,6 +16,8 @@ OD = 1.62
 positiveAngleOffset = 0
 negativeAngleOffset = 0
 ODList = [OD,OD,OD,OD]
-fudgepos = input('Enter fudge positive')
-fudgeneg = input('Enter fudge negative')
->>>>>>> 700673409c2e86bc329bfc9ecc593745aaf1ee14
+#fudgepos = input('Enter fudge positive')
+#fudgeneg = input('Enter fudge negative')
+fudgepos = 2
+fudgeneg = 0.7
+
