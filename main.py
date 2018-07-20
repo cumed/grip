af5c34d3196cd59680c36fa0215c515f60ef576d
cumed <cumedgithub@gmail.com>
2018-07-20 11:10:05 -0400

    try 2mm on positive

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ import os
 currDir = os.path.dirname(os.path.realpath('__file__'))
 filename = os.path.join(currDir,'npy//JL4_2mm.npy')
 directions = np.load(filename)
-#directions[:,1] = -1*directions[:,1]
+directions[:,1] = -1*directions[:,1]
 #new_directions=[]
 #for ele in directions[:,1]:
 #    if ele>-0.001 and ele<0.001: 
