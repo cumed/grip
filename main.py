59eeffa5a9e34ace6220c6c50faa13933b980b39
cumed <cumedgithub@gmail.com>
2018-07-20 11:36:02 -0400

    Meritmedical bend 2mm

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ import os
 #import factors as fact
 #%% Define directions and thresholds
 currDir = os.path.dirname(os.path.realpath('__file__'))
-filename = os.path.join(currDir,'npy//JL4_2mm.npy')
+filename = os.path.join(currDir,'npy//MeritMedical2.npy')
 directions = np.load(filename)
-directions[:,1] = -1*directions[:,1]
+#directions[:,1] = -1*directions[:,1]
 #new_directions=[]
 #for ele in directions[:,1]:
 #    if ele>-0.001 and ele<0.001: 
