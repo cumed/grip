b16c32fa580a22f0bf323a71bb689857600b1bf1
cumed <cumedgithub@gmail.com>
2018-07-24 15:51:45 -0400

    JL4 proper test with new pins

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import os
 #import factors as fact
 #%% Define directions and thresholds
 currDir = os.path.dirname(os.path.realpath('__file__'))
-filename = os.path.join(currDir,'npy/MeritMedicalScaled2.npy')
+filename = os.path.join(currDir,'npy/JL4_2mm.npy')
 #filename = os.path.join(currDir,'npy/JL4_2mod.npy')
 directions = np.load(filename)
 
