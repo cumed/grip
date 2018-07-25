457f56a3a890590ae360000fba2b30e8e623169e
cumed <cumedgithub@gmail.com>
2018-07-25 16:33:23 -0400

    a

--- factors.py
+++ factors.py
@@ -10,11 +10,11 @@ Created on Fri Jul 20 15:24:37 2018
 
 
 #%% 
-bendPinsFactorPos = input('Enter bend pins factor positive?')
-#bendPinsFactorPos = 1.5
+#bendPinsFactorPos = input('Enter bend pins factor positive?')
+bendPinsFactorPos = 2.15
 #bendPinsFactorPos = 0
-#bendPinsFactorNeg = 0
-bendPinsFactorNeg = input('Enter bend pins factor negative')
+bendPinsFactorNeg = 1.95
+#bendPinsFactorNeg = input('Enter bend pins factor negative')
 ##bendPinsFactor = 1.65
 angleRedFactor = 1
 distanceFactor = 1
@@ -24,12 +24,14 @@ OD = 1.62
 positiveAngleOffset = 0
 negativeAngleOffset = 0
 ODList = [OD,OD,OD,OD]
-#fudgepos = input('Enter fudge positive')
-#fudgeneg = input('Enter fudge negative')
-fudgepos = 1
-#fudgeposFour = 1
-fudgeposFour = fudgepos+0
-#fudgeneg = input('Enter fudge negative')
-fudgeneg = 1
-fudgenegFour = fudgeneg+0
 xDistPins = 3
+
+fudgepos = input('Enter fudge positive')
+#fudgepos = 1
+fudgeposFour = fudgepos+0
+
+fudgeneg = input('Enter fudge negative')
+#fudgeneg = 1
+fudgenegFour = fudgeneg+0
+
+
