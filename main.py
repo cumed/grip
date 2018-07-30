4a0b4e165ca772f97b23f77f152e7d42c3a4e652
cumed <cumedgithub@gmail.com>
2018-07-30 15:04:52 -0400

    a

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ currDir = os.path.dirname(os.path.realpath('__file__'))
 filename = os.path.join(currDir,'npy/longCurve15.npy')
 directions = np.load(filename)
 
+directions[76:,1] = 0
+
 #directions[:,1] = -1*directions[:,1]
 
 #%% Variables that can be changed
