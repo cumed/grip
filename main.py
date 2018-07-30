d08b6ceaaf60580b47557ed65baeb07567f3c140
cumed <cumedgithub@gmail.com>
2018-07-30 15:35:18 -0400

    a

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import os
 #import factors as fact
 #%% Define directions and thresholds
 currDir = os.path.dirname(os.path.realpath('__file__'))
-filename = os.path.join(currDir,'npy/23mmpigtail25.npy')
+filename = os.path.join(currDir,'npy/23mmpigtail2.npy')
 directions = np.load(filename)
 
 
