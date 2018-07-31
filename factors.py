dd62e06b69e093cd05181f52cb61e113586c900f
cumed <cumedgithub@gmail.com>
2018-07-31 10:42:02 -0400

    a

--- factors.py
+++ factors.py
@@ -15,12 +15,12 @@ rotationalAngle_threshold = 1
 #catheter_ID = 3
 
 #%% 
-bendPinsFactorPos = float(input('Enter bend pins factor positive?')) 
-#bendPinsFactorPos = 2.1
+#bendPinsFactorPos = float(input('Enter bend pins factor positive?')) 
+bendPinsFactorPos = 2.05
 #bendPinsFactorPos = 0
 #bendPinsFactorNeg = 1.95
-#bendPinsFactorNeg = 1.8
-bendPinsFactorNeg = float(input('Enter bend pins factor negative'))
+bendPinsFactorNeg = 2.0
+#bendPinsFactorNeg = float(input('Enter bend pins factor negative'))
 
 angleRedFactor = 1
 distanceFactor = 1
@@ -32,7 +32,7 @@ negativeAngleOffset = 0
 ODList = [OD,OD,OD,OD]
 xDistPins = 3
 
-a = input('Enter fudge factor for small angles')
+a = 5
 fudgepos = input('Enter fudge positive')
 #fudgepos = 1.4
 fudgeposFour = fudgepos+a
