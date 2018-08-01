7e831f241edaf434ecccf23876590be7c57c73c0
cumed <cumedgithub@gmail.com>
2018-08-01 09:54:34 -0400

    jl4

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import os
 import factors as fact
 #%% Define directions and thresholds
 currDir = os.path.dirname(os.path.realpath('__file__'))
-filename = os.path.join(currDir,'npy/MeritMedicalScaled2.npy')
+filename = os.path.join(currDir,'npy/JL4_2mm.npy')
 directions = np.load(filename)
 
 print(directions[2:12,1])
