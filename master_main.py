7331621ae804d1392028d3f00324b2522ee87331
cumed <cumedgithub@gmail.com>
2018-08-06 15:05:22 -0400

    slash

--- master_main.py
+++ master_main.py
@@ -16,9 +16,10 @@ currDir = os.path.dirname(os.path.realpath('__file__'))
 
 check = int(input('Are the distances and angles in an SVG file or Excel file? \n Press 1 if SVG \n Press 2 if Excel \n'))
 if check ==1:
-    svg_fileName = str(input('Enter the name of the svg file without the file extension .svg \n')+'.svg')
-    svg_Dir = os.path.join(currDir,'svgs')                                        # Combines the current directory path with svgs
-    svg_fileName = os.path.join(svg_Dir,svg_fileName)
+    svg_fileName = input('Enter the name of the svg file without the file extension .svg \n')
+#    svg_Dir = os.path.join(currDir,'svgs')                                        # Combines the current directory path with svgs
+#    svg_fileName = os.path.join(svg_Dir,svg_fileName)
+    svg_fileName = 'svg/'+ str(svg_fileName) + '.svg'
     [x,y] = shape.svg_to_points(svg_fileName)                                     # Obtains the x and y coordinates
     data = np.vstack((x,y,np.zeros(len(x)))).T                                    # For now, 0s are appended for the z dimension
     directions = angle.return_bends(data)                                         # storing the final array of distances, bend and rotational angles
