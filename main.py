9dfed2223599efe47a758ebbdc3dcf43b6f4a3f5
cumed <cumedgithub@gmail.com>
2018-08-06 11:20:22 -0400

    comments

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ currDir = os.path.dirname(os.path.realpath('__file__'))
 check = int(input('If running this file standalone, then press 1. Otherwise press 0 if running master_main.py and hit enter \n'))
 if check==1:
     print('You are running this file by itself.\n')
-    fileStringName = str(input('Enter the npy file name without the \
-                               file extension.\nFor example: 26mmpigtail2.npy should be entered as 26mmpigtail2 \n')+'.npy')
-    filename = os.path.join(currDir,'npy')
-    filename = os.path.join(filename,fileStringName)
+#    fileStringName = str(input('Enter the npy file name without the \
+#                               file extension.\nFor example: 26mmpigtail2.npy should be entered as 26mmpigtail2 \n')+'.npy')
+#    filename = os.path.join(currDir,'npy')
+#    filename = os.path.join(filename,fileStringName)
     directions = np.load('npy/JL4_3mm.npy')
 else:
     filename = os.path.join(currDir,'npy/runfile.npy')
