553a9b5a57015b6e16defaf55f9efcbf2380705e
cumed <cumedgithub@gmail.com>
2018-08-15 15:03:33 -0400

    a

--- control_grippers_for_test.py
+++ control_grippers_for_test.py
@@ -117,7 +117,7 @@ def bendAngle_to_bendDist(angle,outer_diameter):
     #to hit the catheter and bend it by the bending angle to obtain the right
     #shape and thereby convert that distance to the pulse   
     if angle>0:
-        x_i = (d_pins - outer_diameter)/2 - fact.bendPinsFactorPos - d_pins                                    # Distance the pin has to move to touch the catheter
+        x_i = (d_pins - outer_diameter)/2 - fact.bendPinsFactorPos                                     # Distance the pin has to move to touch the catheter
     elif angle<0:
         x_i = (d_pins - outer_diameter)/2 - fact.bendPinsFactorNeg
     else:
@@ -128,6 +128,7 @@ def bendAngle_to_bendDist(angle,outer_diameter):
             x_i = (d_pins - outer_diameter)/2 - fact.bendPinsFactorNeg
 
     bendDist = x_i + y_i *math.tan(math.radians(abs(angle)))*fudge_func(angle)         # x_i + the distance for the supposed bend
+    print(bendDist)
     if math.isnan(bendDist):
         print('Gonna crash here. Angle:'+str(angle))
     return bendDist
